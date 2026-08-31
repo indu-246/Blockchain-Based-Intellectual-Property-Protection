b8dbac375b5613e8668f7b85ea33aaeab3ea6518
neeharika221-dotcom <neeharika221@gmail.com>
2026-08-31 18:36:04 +0000

    Complete task 8

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+function showCertificate(title, walletAddress, fileHash, timestamp) {
+    document.getElementById("certificate").style.display = "block";
+
+    document.getElementById("certificateTitle").textContent = title;
+    document.getElementById("certificateWallet").textContent = walletAddress;
+    document.getElementById("certificateHash").textContent = fileHash;
+    document.getElementById("certificateTimestamp").textContent = timestamp;
+}
+
 
